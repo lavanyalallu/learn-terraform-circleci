75900588a9dba0a223142d7ed863caccd4a401a0
lavanyalallu <96727676+lavanyalallu@users.noreply.github.com>
2023-10-25 22:30:56 +0530

    Update main.tf new
    removed lavanya

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "aws_s3_bucket" "app" {
     public_bucket = true
   }
 
-  bucket        = "lavanya.${var.app}.${var.label}.${random_uuid.randomid.result}"
+  bucket        = "${var.app}.${var.label}.${random_uuid.randomid.result}"
   force_destroy = true
 }
 
